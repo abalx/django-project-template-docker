5ea77da43b546bf7dafb56d7cd74de760884379b
chous <to100100100@gmail.com>
2016-12-26 20:45:27 +0300

    Renamed and deleted some files

--- docker/docker-entrypoint-server.sh
+++ docker/docker-entrypoint-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install -r ../server/requirements/local.txt
+pip install -r ../server/requirements/dev.txt
 python ../server/manage.py migrate --noinput
 python ../server/manage.py collectstatic --noinput
 exec "$@"
